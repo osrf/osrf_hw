0d3261bd2279a5b8dc765a63a26ee6065b20fffd
Mikael Arguedas <mikael.arguedas@gmail.com>
2015-07-15 18:53:52 -0700

    added step extension and lowercase comparison

--- kicad_scripts/freecadconvert.py
+++ kicad_scripts/freecadconvert.py
@@ -6,13 +6,13 @@ import os
 #path ="/home/mikael/testlibkicad/530480210.stp"
 path = "/home/mikael/testlibkicad/microusb/473460001.stp"
 #path = args.input
-directory = "/home/mikael/testlibkicad/"
+directory = "/home/mikael/kicad_ws/osrf_hw/kicad_3dmodels"
 if not os.path.isdir(directory):
     print "ERROR directory " + directory + " doesn't exist"
     exit(1)
 for root, subdirs, files in os.walk(directory):
     for filename in files:
-        if filename.endswith('.stp'):
+        if filename.lower().endswith('.stp') or filename.lower().endswith('.step'):
             file_path = os.path.join(root,filename)
             ImportGui.open(file_path)
             wrlname = file_path[0:file_path.rfind(file_path[file_path.rfind('.'):])] + ".wrl"
