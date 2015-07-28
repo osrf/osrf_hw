9bb818f8e4c836dd60423cd972ee2658cb8f2207
Mikael Arguedas <mikael.arguedas@gmail.com>
2015-07-27 20:08:05 -0700

    added bom plugins and kicad-library-utils during configuration

--- kicad_scripts/configure_kicad.sh
+++ kicad_scripts/configure_kicad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#TODO write BOM plugin in eeschema file
-#TODO install kicad-library-utils scripts
+#TODO add options and function: --update_repos,--update_config
+#TODO security check if files exist in ~/.confic/kicad 
 detect_pretty_repos()
 {
     # Check for the correct option to enable extended regular expressions in
@@ -67,7 +67,6 @@ done
 
 DIR=$( dirname "$DIR" )
 WORKING_TREES=$DIR
-#echo "$WORKING_TREES"
 
 DIRS="3dmodels modules libraries"
 for dirtocreate in $DIRS;do
@@ -84,20 +83,31 @@ echo $MODULE_DIR
 ### clone osrf and kicad pretty repositories
 #FIXME not use kicatTools organization ?
 #checkout_or_update_libraries "kicadTools KiCad"
+
 checkout_or_update_libraries "KiCad"
 
 
-### clone/update kicad-library repo
-if [ ! -e "$WORKING_TREES/kicad-library" ];then
-    git clone "https://github.com/KiCad/kicad-library.git" "$WORKING_TREES/kicad-library"
-else
-    cd "$WORKING_TREES/kicad-library"
-    git pull
-fi
+## clone/update kicad-library repo
+
+# cloning my fork but we can clon the official organisation #orga="KiCad"
+ORGA="keulYSMB"
+REPOS="kicad-library kicad-library-utils"
+for repo in $REPOS;do
+    if [ ! -e "$WORKING_TREES/$repo" ];then
+        git clone "https://github.com/$ORGA/$repo" "$WORKING_TREES/$repo"
+    else
+        cd "$WORKING_TREES/$repo"
+        git pull
+    fi
+done
 # copy content in workspace resources
 cp -r "$WORKING_TREES/kicad-library/library"/* "$LIB_DIR"
 #FIXME should copy only the .3dshape folders ?
 cp -r "$WORKING_TREES/kicad-library/modules/packages3d"/* "$MODELS_DIR"
+
+# install kicad parsing python classes
+cd "$WORKING_TREES/kicad-library-utils"
+sudo python setup.py install
 
 MODULE_LIST=$(ls $MODULE_DIR)
 FILENAME=$HOME/.config/kicad/fp-lib-table
@@ -126,3 +136,13 @@ for key in ${!arr[@]};do
     fi
 done
 
+# add the BOM scripts to eeschema
+EESCHEMAFILE=$HOME/.config/kicad/eeschema
+declare -A arr=( ["FieldNames"]="(templatefields (field (name MFN)(value _)) (field (name MFP)(value _)) (field (name D1)(value digikey)) (field (name D2)(value mouser)) (field (name D1PN)(value _)) (field (name D1PL)(value _)) (field (name D2PN)(value _)) (field (name D2PL)(value _)) (field (name Package)(value _)) (field (name Description)(value _) visible) (field (name Voltage)(value _)) (field (name Power)(value _)) (field (name Tolerance)(value _)) (field (name Temperature)(value _)) (field (name ReverseVoltage)(value _)) (field (name ForwardVoltage)(value _)) (field (name Cont.Current)(value _)) (field (name Frequency)(value _)) (field (name \"Impedence(Peak)\")(value _)) (field (name ResonnanceFreq)(value _)))" ["bom_plugins"]="(plugins  (plugin generateBOM (cmd \"python \\\\\\\\\"$WORKING_TREES/osrf_hw/kicad_scripts/editSch.py\\\\\\\\\" \\\\\\\\\"%I\\\\\\\\\" \\\\\\\\\"%O.csv\\\\\\\\\" generate\"))  (plugin editBOM_SCH_PyQt (cmd \"python \\\\\\\\\"$WORKING_TREES/osrf_hw/kicad_scripts/editSch.py\\\\\\\\\" \\\\\\\\\"%I\\\\\\\\\" \\\\\\\\\"%O\\\\\\\\\" edit\")))" ["bom_plugin_selected"]="editBOM_SCH_PyQt")
+for key in ${!arr[@]};do
+    if grep -q "$key *=" $EESCHEMAFILE; then
+        sed -i "s#\(${key} *= *\).*#\1${arr[${key}]}#" $EESCHEMAFILE
+    else
+        echo "$key=${arr[${key}]}" >> $EESCHEMAFILE
+    fi
+done
