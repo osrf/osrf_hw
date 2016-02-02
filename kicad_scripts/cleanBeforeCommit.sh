c02f9626d6beaf2eb03b37b2325b20b405295e95
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-02-01 17:33:49 -0800

    fixed removeCacheLib function

--- kicad_scripts/cleanBeforeCommit.sh
+++ kicad_scripts/cleanBeforeCommit.sh
@@ -5,61 +5,75 @@ b="osrf_hw"
 cd $1
 pwd
 #DIRECTORYTEST=$1
-libDirLine="LibDir=\${KISYSLIB}"
 
-b="osrf_hw"
-c="../../$b"
-cc=$(find $1 -type f -name "*.pro")
-for file in $cc
-do
-  cd "$( dirname $file)"
+remove_backup_files()
+{
   rm *bak *.bck *.swp *-cache.lib
-##DIRWORKSPACE=$( dirname "$DIRECTORYTEST")
-  outString=""
-  first=0
-  while IFS='' read -r line || [[ -n "$line" ]]; do
-      if [[ $line == LibName* ]] && [[ $line == *osrf_hw* ]] && [[ $line != ../../osrf_hw* ]];
-      then
-        libnumber=${line%=*}
-        line2=${line#*=}
-        line3=${line2#*osrf_hw}
-        line="$libnumber=$c$line3"
-      fi
-      if [[ $line == LibDir* ]] && [[ $line != $libDirLine ]];
-      then
-        line=$libDirLine
-      fi
-      if [[ $first != 0 ]]
-      then
-        outString=$outString"\n"
-      fi
-      outString=$outString"$line"
-      first=1
+}
+
+makeLibPathsRelative()
+{
+  libDirLine="LibDir=\${KISYSLIB}"
+  b="osrf_hw"
+  c="../../$b"
+  cc=$(find $1 -type f -name "*.pro")
+  for file in $cc
+  do
+    cd "$( dirname $file)"
+    outString=""
+    first=0
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [[ $line == LibName* ]] && [[ $line == *osrf_hw* ]] && [[ $line != ../../osrf_hw* ]];
+        then
+          libnumber=${line%=*}
+          line2=${line#*=}
+          line3=${line2#*osrf_hw}
+          line="$libnumber=$c$line3"
+        fi
+        if [[ $line == LibDir* ]] && [[ $line != $libDirLine ]];
+        then
+          line=$libDirLine
+        fi
+        if [[ $first != 0 ]]
+        then
+          outString=$outString"\n"
+        fi
+        outString=$outString"$line"
+        first=1
+    done
   done < $file
   echo -e $outString > $file 
-done
+}
 
-#cc=$(find $1 -type f -name "*.sch")
-#outString=""
-#for file in $cc
-#do
-#  echo $file
-#  cd "$( dirname $file)"
-#  first=0
-#  while IFS='' read -r line || [[ -n "$line" ]]; do
-#      if [[ $first != 0 ]]
-#      then
-#        outString=$outString"\n"
-#      fi
-#      if [[ $line == LIBS* ]] && [[ $line == *-cache ]];then
-#          :
-#      else
-#          outString=$outString"$line"
-#          first=1
-#      fi
-#  done < $file
-#  echo -e $outString > $file
-#done
+removeCacheLib()
+{
+  cc=$(find $1 -type f -name "*.sch")
+  for file in $cc
+  do
+    outString=""
+    echo $file
+    cd "$( dirname $file)"
+    first=0
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [[ $line == LIBS* ]] && [[ $line == *-cache ]];
+        then
+            echo $line
+        else
+          if [[ $first != 0 ]]
+          then
+            outString=$outString"\n"
+          fi
+          outString=$outString"$line"
+          first=1
+        fi
+    done < $file
+    echo -e $outString > $file
+  done
+}
+
+remove_backup_files
+makeLibPathsRelative
+removeCacheLib
 
 #FIXME Chunk of code to find osrf_hw and workspace: useless because workspace structure is fixed
 #DIRECTORYTEST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
